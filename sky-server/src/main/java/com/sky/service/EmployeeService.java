4e41ac62555de24506426e4768c811af47fa1436
七度光阴 <257960659@qq.com>
2025-04-07 22:00:06 +0800

    新增员工

---
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -0,0 +1,20 @@
+package com.sky.service;
+
+import com.sky.dto.EmployeeLoginDTO;
+import com.sky.entity.Employee;
+
+public interface EmployeeService {
+
+    /**
+     * 员工登录
+     * @param employeeLoginDTO
+     * @return
+     */
+    Employee login(EmployeeLoginDTO employeeLoginDTO);
+
+    /**
+     * 新增员工实现方法
+     * @param employeeLoginDTO
+     */
+    void save(EmployeeLoginDTO employeeLoginDTO);
+}
